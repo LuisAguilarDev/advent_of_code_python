9c289393a6cc55e26957626390e0668ba402c476
LUIS GERARDO AGUILAR GARCIA <100291529+LuisAguilarDev@users.noreply.github.com>
2025-01-25 14:21:40 -0500

    feat: day 1 and zip explain

--- src/2024/day1/day1.py
+++ src/2024/day1/day1.py
@@ -1,3 +1,20 @@
-print("Hello, World",sep = ",")
-print("ja","je",sep = ",",end = "! ")
-print("ja","je",sep = ",",end = "! ")
+import os
+import re
+
+file_path = os.path.join(os.path.dirname(__file__), "day1.txt")
+
+with open(file_path, "r") as file:
+    contents = file.read().splitlines()
+
+# Part 1
+lists = list(map(list,zip(*[list(map(int,line.split())) for line in contents])))
+for l in lists: l.sort()
+result = sum([abs(x-y) for x,y in zip(*lists)])
+
+assert(result == 2375403)
+
+# Part 2
+l,r = list(map(list,zip(*[list(map(int,line.split())) for line in contents])))
+result2 = sum([x * r.count(x) for x in l])
+
+assert(result2 == 23082277)
