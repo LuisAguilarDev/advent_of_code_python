2669189b626ae073dbe306e1a49ff77babf93050
Luis Gerardo Aguilar <100291529+LuisAguilarDev@users.noreply.github.com>
2025-11-08 23:25:32 +0000

    feat: refactor Day 4 scratchcards logic for improved readability and structure

--- src/2023/day4/index.py
+++ src/2023/day4/index.py
@@ -1,49 +1,68 @@
-import os
+from global_utils.utils import read_file
+from global_utils.logger import logger
 
-file_path = os.path.join(os.path.dirname(__file__), "input.txt")
-sample_path = os.path.join(os.path.dirname(__file__), "sample.txt")
-with open(file_path, "r") as file:
-    contents = file.read().splitlines()
-with open(sample_path, "r") as file:
-    sample_contents = file.read().splitlines()
+logger.info("---- Day 4: Scratchcards ----")
 
-result = 0
-for line in contents:
-    pairs = line.split(":")[1].split("|")
-    winning_list = pairs[0].strip().split()
-    having_list = pairs[1].strip().split()
-    winning = set(map(int, winning_list))
-    having = set(map(int, having_list))
-    # intersection of winning and having
-    intersection = winning.intersection(having)
-    if len(intersection) == 0:
-        continue
-    result += 2 ** (len(intersection) - 1)
+contents = read_file("input.txt")
+sample_contents = read_file("sample.txt")
 
+logger.info("Part 1")
+
+
+def get_cards(contents: list[str]):
+    cards = list()
+    for line in contents:
+        pairs = line.split(":")[1].split("|")
+        winning_list = pairs[0].strip().split()
+        having_list = pairs[1].strip().split()
+        winning = set(map(int, winning_list))
+        having = set(map(int, having_list))
+        cards.append((winning, having))
+    return cards
+
+
+cards = get_cards(contents)
+
+
+def sum_of_worth(cards: list[tuple[set[int], set[int]]]) -> int:
+    result = 0
+    for winning, having in cards:
+        intersection = winning.intersection(having)
+        if len(intersection) == 0:
+            continue
+        result += 2 ** (len(intersection) - 1)
+    return result
+
+
+result = sum_of_worth(cards)
+logger.info(f"Total worth of all scratchcards is {result}")
 assert (28538 == result)
 
-# Part 2
-cards = dict()  # dict<card_num, num_of_cards>
-total_cards = 0
+logger.info("Part 2")
 
-for num, line in enumerate(contents, start=1):
-    card_num = num
-    num_cards = cards.get(card_num, 1)
-    total_cards += num_cards
-    pairs = line.split(":")[1].split("|")
-    winning_list = pairs[0].strip().split()
-    having_list = pairs[1].strip().split()
-    winning = set(map(int, winning_list))
-    having = set(map(int, having_list))
-    num_winned = len(winning.intersection(having))
-    win_index = card_num + 1
-    while num_winned > 0:
-        if win_index > len(contents):
-            break
-        if cards.get(win_index) is None:
-            cards[win_index] = 1
-        cards[win_index] += num_cards
-        num_winned -= 1
-        win_index += 1
 
-assert (total_cards == 9425061)
+def total_cards_end_up(cards: list[tuple[set[int], set[int]]]) -> int:
+    cards_dict = dict()  # dict<card_num, num_of_cards>
+    total_cards = 0
+
+    for num, cards_info in enumerate(cards, start=1):
+        card_num = num
+        num_cards = cards_dict.get(card_num, 1)
+        total_cards += num_cards
+        winning, having = cards_info
+        num_winned = len(winning.intersection(having))
+        win_index = card_num + 1
+        while num_winned > 0:
+            if win_index > len(contents):
+                break
+            if cards_dict.get(win_index) is None:
+                cards_dict[win_index] = 1
+            cards_dict[win_index] += num_cards
+            num_winned -= 1
+            win_index += 1
+    return total_cards
+
+
+result = total_cards_end_up(cards)
+logger.info(f"Total number of scratchcards end up is {result}")
+assert (result == 9425061)
