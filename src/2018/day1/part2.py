e7997bcf083adcaf28658da4cb774c098181a2da
Luis Gerardo Aguilar <100291529+LuisAguilarDev@users.noreply.github.com>
2026-01-29 04:23:16 +0000

    fix: simplify logic to get the result on day 1 2018

--- src/2018/day1/part2.py
+++ src/2018/day1/part2.py
@@ -3,23 +3,22 @@ from global_utils.logger import logger
 
 
 # python understand _ on large numbers and removes them and they improve readability for humans
-def get_first_repeated_total(numbers, max_cycles=1_000_000) -> int:
+def get_first_repeated_total(numbers) -> int:
     """
     Detects the first coincident value on a loop and returns it
     
     numbers is a list of numbers
-    max_cycles is the max number of iterations to avoid an infinite loop if no coincident number is found.
     """
     if len(numbers) == 0:
         return 0
     seen = set([0])
-    total = 0
-    for _ in range(max_cycles):
+    value = 0
+    while True:
         for number in numbers:
-            total += int(number)
-            if total in seen:
-                return total
-            seen.add(total)
+            value += int(number)
+            if value in seen:
+                return value
+            seen.add(value)
 
 def do_part_2() -> bool:
     logger.info(f"Part 2")
